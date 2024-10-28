5b854df6c2a87e8e9659979ecebb72f62ba34e3f
Javi Martinez <javier.martinez.snt@gmail.com>
2024-10-28 18:03:13 +0100

    add connection db

---
+++ backend/database/db.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"fidelity-client-app/config"
+	"fidelity-client-app/models"
+
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+
+	dsn := fmt.Sprintf(
+		"user=%s password=%s dbname=%s host=%s port=%s",
+		config.Vars.DBUser,
+		config.Vars.DBPass,
+		config.Vars.DBName,
+		config.Vars.DBHost,
+		config.Vars.DBPort,
+	)
+
+	var err error
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	DB.AutoMigrate(
+		&models.User{},
+	)
+
+	return DB
+}
