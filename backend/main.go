e7d89f5cb8c6b52b6f00d0fc32d3730ea416d13a
Javi Martinez <javier.martinez.snt@gmail.com>
2024-10-30 19:26:24 +0100

    add promotion endpoints

--- backend/main.go
+++ backend/main.go
@@ -6,27 +6,37 @@ import (
 	"fidelity-client-app/handlers"
 	"fidelity-client-app/services"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 
 	// Cargar el fichero .env
 	config.LoadEnv()
+
 	// Conectarnos a base de datos
 	DB := database.ConnectDB()
 
+	// Inicializar servicios
 	authService := services.AuthService{DB: DB}
-	authHandler := handlers.AuthHandler{
-		AuthService: authService,
-	}
+	promotionService := services.PromotionService{DB: DB}
+
+	// Inicializar handlers
+	authHandler := handlers.AuthHandler{AuthService: authService}
+	promotionHandler := handlers.PromotionHandler{PromotionService: &promotionService}
+
 	// Iniciar enrutador
-	r := mux.NewRouter()
+	mux := http.NewServeMux()
 
-	// Rutas publicas
-	r.HandleFunc("/api/v1/register", authHandler.RegisterNewUser).Methods("POST")
-	r.HandleFunc("/api/v1/login", authHandler.LoginUser).Methods("POST")
+	// Rutas publicas de Auth
+	mux.HandleFunc("/api/v1/register", authHandler.RegisterNewUser)
+	mux.HandleFunc("/api/v1/login", authHandler.LoginUser)
 
-	http.ListenAndServe(":8080", r)
+	// Rutas para promociones
+	mux.HandleFunc("/api/v1/promotions/create", promotionHandler.CreatePromotion)     // POST: Crear promoción
+	mux.HandleFunc("/api/v1/promotions/active", promotionHandler.GetActivePromotions) // GET: Obtener promociones activas (con paginación)
+	mux.HandleFunc("/api/v1/promotions", promotionHandler.GetPromotionByID)           // GET: Obtener promoción por ID
+	mux.HandleFunc("/api/v1/promotions/update", promotionHandler.UpdatePromotion)     // PUT: Actualizar promoción
+	mux.HandleFunc("/api/v1/promotions/delete", promotionHandler.DeletePromotion)     // DELETE: Eliminar promoción
+
+	http.ListenAndServe(":8080", mux)
 }
