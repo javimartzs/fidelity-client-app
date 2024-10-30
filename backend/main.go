1aa95a4f39e7426a638f02506d744fad78f5dbcf
Javi Martinez <javier.martinez.snt@gmail.com>
2024-10-30 21:23:36 +0100

    add promotionusage endpoints

--- backend/main.go
+++ backend/main.go
@@ -34,11 +34,14 @@ func main() {
 	mux.HandleFunc("/api/v1/login", authHandler.LoginUser)
 
 	// Rutas para promociones
-	mux.HandleFunc("/api/v1/promotions/create", promotionHandler.CreatePromotion)     // POST: Crear promoción
-	mux.HandleFunc("/api/v1/promotions/active", promotionHandler.GetActivePromotions) // GET: Obtener promociones activas (con paginación)
-	mux.HandleFunc("/api/v1/promotions", promotionHandler.GetPromotionByID)           // GET: Obtener promoción por ID
-	mux.HandleFunc("/api/v1/promotions/update", promotionHandler.UpdatePromotion)     // PUT: Actualizar promoción
-	mux.HandleFunc("/api/v1/promotions/delete", promotionHandler.DeletePromotion)     // DELETE: Eliminar promoción
+	mux.HandleFunc("/api/v1/promotions/create", promotionHandler.CreatePromotion)                     // POST: Crear promoción
+	mux.HandleFunc("/api/v1/promotions/active", promotionHandler.GetActivePromotions)                 // GET: Obtener promociones activas (con paginación)
+	mux.HandleFunc("/api/v1/promotions", promotionHandler.GetPromotionByID)                           // GET: Obtener promoción por ID
+	mux.HandleFunc("/api/v1/promotions/update", promotionHandler.UpdatePromotion)                     // PUT: Actualizar promoción
+	mux.HandleFunc("/api/v1/promotions/delete", promotionHandler.DeletePromotion)                     // DELETE: Eliminar promoción
+	mux.HandleFunc("/api/v1/promotions/active_for_user", promotionHandler.GetActivePromotionsForUser) // Promociones activas no consumidas por usuario
+	mux.HandleFunc("/api/v1/promotions/consume", promotionHandler.ConsumePromotion)                   // Consumir promoción
+	mux.HandleFunc("/api/v1/promotions/check", promotionHandler.CheckPromotionAvailability)           // Verificar si la promoción ha sido consumida
 
 	// Ruta para acumulación de puntos
 	mux.HandleFunc("/api/v1/accumulate_points", pointsHandler.AccumulatePoints)
